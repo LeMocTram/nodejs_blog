6bfbb157a9311dd581b223eda2c441be145b7bec
Moc Tram <19522373@gm.uit.edu.vn>
2023-11-11 11:03:00 +0700

    static file & scss

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
-const port = 3000
+const port = 3001
 
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
 app.use(morgan('combined'))
 
 
 //Template engine
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname,'resources/views'))
+app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
 console.log()
 app.get('/', (req, res) => {
   res.render('home');
-})  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
